eb814d7bec129e7c7cad2ca97ecc13ce8df0d0c7
Brett Cunningham <bwcunninghamii@gmail.com>
2018-11-19 21:48:01 -0500

    Added project and tested Hello World

---
+++ SpeechToText/SpeechToText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToText
+//
+//  Created by Brett Cunningham on 11/19/18.
+//  Copyright © 2018 Altruistic Software Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
